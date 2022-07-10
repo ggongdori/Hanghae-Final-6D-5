7442b9fcce5acd80da644f29bf2a6d32c0e065aa
ggongdori <soominlee0410@gmail.com>
2022-07-10 22:19:20 +0900

    [refactoring]

--- src/main/java/com/hanghae/justpotluck/domain/alarm/controller/AlarmController.java
+++ src/main/java/com/hanghae/justpotluck/domain/alarm/controller/AlarmController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AlarmController {
     private final AlarmService alarmService;
-
     /* 알림 읽음 확인 */
     @PostMapping("/api/alarm/{alarmId}")
     public AlarmResponseDto alarmReadCheck(
